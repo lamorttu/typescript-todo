504c6b1c27ae2bb5c893bacf4e29ac6b9e9c06f5
lamorttu <lamorttu@gmail.com>
2020-05-06 23:19:40 +0800

    [other] function&&generics-learn

--- src/example/function.ts
+++ src/example/function.ts
@@ -5,3 +5,32 @@ const addFunc2 = (arg1: number, arg2: number) => arg1 + arg2;
 let addFunc3: (x: number, y: number) => number;
 addFunc3 = (arg1: number, arg2: number): number => arg1 + arg2;
 // addFunc3 = (arg1: string, arg2: number): number => arg1 + arg2;
+
+type Add = (x: number, y: number) => number;
+type isString = string;
+let addFunc4: Add
+addFunc4 = (arg1: number, arg2: number) => arg1 + arg2;
+// addFunc4 = (arg1, arg2, arg3) => arg1 + arg2 + (arg3 ? arg3 : 0);
+
+type AddFunc2 = (arg1: number, arg2: number, arg3?: number) => number;
+// let addFunc5: AddFunc2;
+// addFunc5 = (x: number, y: number) => x + y;
+// addFunc5 = (x: number, y: number, z: number) => x + y + z;
+let addFunc5 = (x: number, y = 3) => x + y;
+// addFunc5(1, '2');
+
+const handleData = (arg1: number, ...args: number[]) => {
+    // ...
+}
+
+function overLoaded(x: string): string[];
+function overLoaded(x: number): number[];
+function overLoaded(x: any): any {
+    if (typeof x === 'string') {
+        return x.split('')
+    } else {
+        return x.toString().split('')
+    }
+}
+overLoaded('abc');
+overLoaded(1);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import './example/basic-type';
 // import './example/interface';
-import './example/function';
+// import './example/function';
+import './example/generics';
 
 
 document.title = 'Ronnie';
