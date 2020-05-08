1a9a4682ff53d81eb77c5b5742466107420afa3b
lamorttu <lamorttu@gmail.com>
2020-05-08 23:06:38 +0800

    [other] es6-class-learn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // import './example/basic-type';
 // import './example/interface';
 // import './example/function';
-import './example/generics';
-
+// import './example/generics';
+import './example/es6-class.js';
+// import './modules/index';
 
 document.title = 'Ronnie';
